c6c238ac5e3deb4bd743b0f879284e7dabc0e759
harikapadia999 <harikapadia999@gmail.com>
2024-05-12 13:28:04 +0530

    feat :: Add features section and add sentry performance dashboard.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
+import Features from "./components/Features";
 import Navbar from "./components/Navbar";
+import * as Sentry from "@sentry/react";
+import HowItWorks from "./components/HowItWorks";
 
 const App = () => {
   return (
@@ -10,7 +13,9 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
+      <HowItWorks />
     </main>
   );
 };
-export default App;
+export default Sentry.withProfiler(App);
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://0807174cfdfbb47b88ac8fda4ff6e319@o4507242118709248.ingest.de.sentry.io/4507242167926864",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
